fbf473dc069746150f35cdc75ba01b51a12879d9
Luke <l.avveduto00@gmail.com>
2025-10-07 16:18:28 +0200

    Struttura iniziale

---
+++ dec_float/dec.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main() {
+  var decimal_float int;
+  fmt.Print("Ciao Biondo! Inserisci il tuo numero decimale! ");
+  fmt.Scan(&decimal_float);
+  fmt.Println(decimal_float);
+}
