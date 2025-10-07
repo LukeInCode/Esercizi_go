557ca5291b0fee81f5cf4f12080377641ce68eba
Luke <l.avveduto00@gmail.com>
2025-10-07 17:04:47 +0200

    Primo esercizio svolto

--- dec_float/dec.go
+++ dec_float/dec.go
@@ -2,8 +2,9 @@ package main
 import "fmt"
 
 func main() {
-  var decimal_float int;
-  fmt.Print("Ciao Biondo! Inserisci il tuo numero decimale! ");
+  var decimal_float float64;
+  fmt.Print("Ciao Biondo! Inserisci il tuo numero decimale: ");
   fmt.Scan(&decimal_float);
-  fmt.Println(decimal_float);
+  var int_input int = int(decimal_float);
+  fmt.Println(decimal_float - float64(int_input));
 }
