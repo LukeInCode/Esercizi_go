604de78b3adce70f701e9598298b7f7a282fba69
Luke <l.avveduto00@gmail.com>
2025-10-08 15:45:47 +0200

    parabola

---
+++ 081025/calc_parabola.go
@@ -0,0 +1,25 @@
+/*
+  Avveduto Luca
+  Calcolo soluzioni di un'equazione di secondo grado
+*/
+
+package main
+import (
+  "fmt"
+  "math"
+)
+
+func main() {
+  var a,b,c float64;
+
+  fmt.Print("Inserisci il primo coefficiente: ");
+  fmt.Scan(&a);
+  fmt.Print("\nInserisci il secondo coefficiente: ");
+  fmt.Scan(&b);
+  fmt.Print("\nInserisci il terzo coefficiente: ");
+  fmt.Scan(&c);
+
+  var delta float64 = math.Pow(b,2) - 4 * a * c;
+  var x1, x2 float64 = (b * -1 + math.Sqrt(delta)) / (2 * a), (b * -1 - math.Sqrt(delta)) / (2 * a);
+  fmt.Println(x1,x2);
+}
