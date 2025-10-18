56282ef4ab44d33cd3b539178dcf6a10f474285b
Luke <l.avveduto00@gmail.com>
2025-10-18 15:55:22 +0200

    Es.1

--- teoria/171025/giorni_dal_1970/giorni_dal_1970.go
+++ teoria/171025/giorni_dal_1970/giorni_dal_1970.go
@@ -3,8 +3,15 @@ import "fmt"
 
 func main() {
   var giorno, mese, anno int;
+  var giorno_target, mese_target, anno_target = 1,1,1970;
+  const GIORNI_ANNO = 365;
+  const GIORNI_MESE = 31;
+  
+  fmt.Print("Inserire rispettivamente gg mm aaaa: "); fmt.Scan(&giorno,&mese,&anno);
 
-  fmt.Print("Inserire rispettivamente gg mm aaaa"); fmt.Scan(&giorno,&mese,&anno);
+  var giorni_totali int = (anno - anno_target) * GIORNI_ANNO;
+  giorni_totali += (giorno - giorno_target) + ((mese - mese_target) * GIORNI_MESE);
 
+  fmt.Printf("La distanza tra %d/%d/%d e %d/%d/%d è di %d giorni\n", giorno_target,mese_target,anno_target,giorno,mese,anno,giorni_totali);
   
 }
