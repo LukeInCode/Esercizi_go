d20655bce0552ebeed47fdbc9f11305b56f66b0c
Luke <l.avveduto00@gmail.com>
2025-10-18 16:26:00 +0200

    Primo esercizio completo

--- teoria/171025/giorni_dal_1970/giorni_dal_1970.go
+++ teoria/171025/giorni_dal_1970/giorni_dal_1970.go
@@ -4,14 +4,39 @@ import "fmt"
 func main() {
   var giorno, mese, anno int;
   var giorno_target, mese_target, anno_target = 1,1,1970;
-  const GIORNI_ANNO = 365;
-  const GIORNI_MESE = 31;
+  
   
   fmt.Print("Inserire rispettivamente gg mm aaaa: "); fmt.Scan(&giorno,&mese,&anno);
 
-  var giorni_totali int = (anno - anno_target) * GIORNI_ANNO;
-  giorni_totali += (giorno - giorno_target) + ((mese - mese_target) * GIORNI_MESE);
-
+  var giorni_totali int = giorno - giorno_target;
+  for i := 0; i < (anno - anno_target); i++ {
+    for j := 1; j <= 12; j++ {
+      if j == 11 || j == 4 || j == 6 || j == 9 {
+        giorni_totali += 30;
+      }else if j == 2 {
+        anno_corrente := anno_target + i;
+        if (anno_corrente % 100 == 0 && anno_corrente % 400 == 0) || anno_corrente % 4 == 0 {
+          giorni_totali += 29;
+        }else {
+          giorni_totali += 28;
+        }
+      }else {
+        giorni_totali += 31;
+      }
+    }
+  }
+  for i := 1; i <= (mese - mese_target); i++ {
+    if i == 11 || i == 4 || i == 6 || i == 9 {
+      giorni_totali += 30;
+    }else if i == 2 {
+      if (anno % 100 == 0 && anno % 400 == 0) || anno % 4 == 0 {
+        giorni_totali += 29;
+      }else {
+        giorni_totali += 28;
+      }
+    }else {
+      giorni_totali += 31;
+    }
+  }
   fmt.Printf("La distanza tra %d/%d/%d e %d/%d/%d è di %d giorni\n", giorno_target,mese_target,anno_target,giorno,mese,anno,giorni_totali);
-  
 }
