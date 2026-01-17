c79e509c2e14496aa8bcfc9782cc0e814a42f0b2
Luke <l.avveduto00@gmail.com>
2026-01-17 12:17:49 +0100

    Ultimo es simulazione

--- temi_esame/tde_250108/appuntamenti/appuntamenti.go
+++ temi_esame/tde_250108/appuntamenti/appuntamenti.go
@@ -1,88 +1,79 @@
 package main
-
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+import "bufio"
+import "os"
+import "strconv"
+import "strings"
 
 type Appuntamento struct {
-	giorno             int
-	oraInizio, oraFine int
+  giorno, oraInizio, oraFine int;
 }
 
-func (a Appuntamento) String() (s string) {
-	prima := strings.Repeat("-", a.oraInizio)
-	app := strings.Repeat("*", a.oraFine-a.oraInizio)
-	dopo := strings.Repeat("-", 24-a.oraFine)
-	return fmt.Sprintf(
-		"[il giorno %d dalle %02d alle %02d: %s%s%s]",
-		a.giorno, a.oraInizio, a.oraFine, prima, app, dopo)
+func NuovoAppuntamento(gg, h1, h2 int) (Appuntamento, bool) {
+  if gg < 1 || gg > 366 {
+    return Appuntamento{}, false;
+  }
+  if h1 > h2 {
+    return Appuntamento{}, false;
+  }
+  if (h1 < 0 || h1 > 24) || (h2 < 0 || h2 > 24) {
+    return Appuntamento{}, false;
+  }
+  return Appuntamento{ giorno: gg, oraInizio: h1, oraFine: h2 }, true;
+}
+
+
+func SovrapposizioneAppuntamenti(app1, app2 Appuntamento) bool {
+  if app1.giorno != app2.giorno {
+    return false;
+  }
+  return app1.oraInizio < app2.oraFine && app2.oraInizio < app1.oraFine;
+}
+
+func AggiungiAppuntamento(app Appuntamento, agenda *[]Appuntamento) bool {
+  var check bool = true;
+  for _,v := range *agenda {
+    if SovrapposizioneAppuntamenti(app, v) {
+      check = false;
+      break;
+    }
+  }
+  if check {
+    *agenda = append(*agenda, app);
+  }
+  return check;
+}
+
+func (app Appuntamento) String() string {
+  var temp_line = "";
+  const HOURS int = 24;
+  
+  for i := 0; i < HOURS; i++ {
+    if i >= app.oraInizio && i < app.oraFine {
+      temp_line += "*";
+      continue;
+    }
+    temp_line += "-";
+  }
+  return fmt.Sprintf("il giorno %.2d dalle %.2d alle %.2d: %s", app.giorno, app.oraInizio, app.oraFine, temp_line);
 }
 
 func main() {
-	var gg, h1, h2 int
-	agenda := make([]Appuntamento, 0)
-	for {
-		_, err := fmt.Scan(&gg, &h1, &h2)
-		if err != nil {
-			break
-		}
-		a, ok := NuovoAppuntamento(gg, h1, h2)
-		if ok {
-			AggiungiAppuntamento(a, &agenda)
-		}
-	}
-	for _, ap := range agenda {
-		fmt.Println(ap)
-	}
-	//fmt.Scan(&gg)
-	//fmt.Println(AppuntamentiGiornata(gg, agenda))
-}
+  var agenda []Appuntamento;
+  scanner := bufio.NewScanner(os.Stdin);
 
-func GiornoValido(gg int) bool {
-	return 1 <= gg && gg <= 366
+  for scanner.Scan() {
+    var params []int;
+    for _,s := range strings.Fields(scanner.Text()) {
+      n,_ := strconv.Atoi(s);
+      params = append(params, n);
+    }
+    app,err := NuovoAppuntamento(params[0], params[1], params[2]);
+    if err {
+      AggiungiAppuntamento(app, &agenda);
+    }
+  }
+  for _,v := range agenda {
+    fmt.Println(v);
+  }
 }
-
-func OraValida(h int) bool {
-	return 0 <= h && h <= 24
-}
-
-func NuovoAppuntamento(gg, h1, h2 int) (a Appuntamento, ok bool) {
-	if GiornoValido(gg) && OraValida(h1) && OraValida(h2) && h1 <= h2 {
-		a = Appuntamento{gg, h1, h2}
-		ok = true
-	} else {
-		ok = false
-	}
-	return
-}
-
-func AggiungiAppuntamento(a Appuntamento, agenda *[]Appuntamento) bool {
-	for _, app := range *agenda {
-		if SovrapposizioneAppuntamenti(a, app) {
-			return false
-		}
-	}
-	*agenda = append(*agenda, a)
-	return true
-}
-
-func SovrapposizioneAppuntamenti(a, b Appuntamento) bool {
-	if a.giorno != b.giorno {
-		return false
-	}
-
-	return a.oraInizio < b.oraFine &&
-		b.oraInizio < a.oraFine
-}
-
-/*
-func AppuntamentiGiornata(gg int, agenda []Appuntamento) (giornata []Appuntamento) {
-	for _, a := range agenda {
-		if a.giorno == gg {
-			giornata = append(giornata, a)
-		}
-	}
-	return
-}
-*/
---
+++ temi_esame/tde_250108/appuntamenti/go.mod
@@ -0,0 +1,3 @@
+module appuntamenti
+
+go 1.25.1
